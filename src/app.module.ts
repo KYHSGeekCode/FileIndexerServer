efc61bab91f27645a3a563ed2c3a186c6f9d27e9
KYHSGeekCode <jourhyang123@gmail.com>
2022-02-15 21:56:05 +0900

    :sparkles: Firestore setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
+import { FirestoreModule } from './firestore/firestore.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    FirestoreModule.forRoot({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        keyFilename: configService.get<string>('KEY_FILE_NAME'),
+        projectId: configService.get<string>('PROJECT_ID'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
